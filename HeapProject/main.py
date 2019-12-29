d40cd55ac48972d4aba6e3c1ff9373a938b8d5a3
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-29 17:56:07 +0300

    linked list is added

--- HeapProject/main.py
+++ HeapProject/main.py
@@ -1,17 +1,29 @@
 import heapq
 
-listOfName = []
-file = open("data.txt", "r")
-line = file.readline()
-line = line.strip().split()
 
-line = line.pop(0)
-while line:
-    listOfName.append(line)
-    line = file.readline()
-    line = line.strip().split()
-    if len(line) != 0:
-        line = line.pop(0)
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.value = None
+
+
+listOfName = []
+fileForHeap = open("data.txt", "r")
+lineForHeap = fileForHeap.readline()
+lineForHeap = lineForHeap.strip().split()
+
+lineForHeap = lineForHeap.pop(0)
+while lineForHeap:
+    listOfName.append(lineForHeap)
+    lineForHeap = fileForHeap.readline()
+    lineForHeap = lineForHeap.strip().split()
+    if len(lineForHeap) != 0:
+        lineForHeap = lineForHeap.pop(0)
 
 print(listOfName)
 heapq.heapify(listOfName)
@@ -22,3 +34,25 @@ heapq.heapify(listOfName)
 print(listOfName)
 heapq.heappush(listOfName, 'Ahmet')
 print(listOfName)
+
+fileForLinkedList = open("data.txt", "r")
+lineForLList = fileForLinkedList.readline()
+lineForLList = lineForLList.strip().split()
+lineForLList = lineForLList.pop(0)
+
+linkedList = LinkedList()
+linkedList.value = Node(lineForLList)
+lineForLList = fileForLinkedList.readline()
+lineForLList = lineForLList.strip().split()
+item = Node(lineForLList)
+linkedList.value.next = item
+while lineForLList:
+    tempValue = item
+    lineForLList = fileForLinkedList.readline()
+    lineForLList = lineForLList.strip().split()
+    if len(lineForLList) != 0:
+        lineForLList = lineForLList.pop(0)
+        item = Node(lineForLList)
+        tempValue.next = item
+
+print(linkedList.value)
